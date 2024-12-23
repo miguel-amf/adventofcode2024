2b0e6d568ec6e405d15fc5133ac2f4c47f9a2ab0
Miguel Montagner <dev@miguelmontagner.com>
2024-12-23 16:10:02 +0000

    refactor code from quiz2 into different methods to prepare for part2

--- src/adventofcode/quizcode/Quiz2.java
+++ src/adventofcode/quizcode/Quiz2.java
@@ -18,44 +18,46 @@ public class Quiz2 {
 
         //each line represents a report. iterating over each.
         for(String report : lines) {
-            //System.out.println(report);
-            //lineCnt++;
-            //if(lineCnt >= 10) break;
-            int unsafe = 0;
-            //break the line into strings for each number
-            String[] values = report.split("\\s+");
-            //set a flag for ascending or descending (-1 decreasing, 1 increasing)
-            int direction;
-            //check first two numbers to check trend
-            if(parseInt(values[0]) < parseInt(values[1])) {
-                //System.out.println("asc");
-                direction = 1;
-            } else {
-                //System.out.println("desc");
-                direction = -1;
-            }
-
-            for(int i = 0; i < values.length - 1; i++) {
-                //convert the number and next one to be compared
-                int current = parseInt(values[i]);
-                int next = parseInt(values[i+1]);
-                //checks for the 4 unsafe scenarios: breaking
-                if ((direction == 1 && current >= next)         ||  // Ascending violation (or equals)
-                    (direction == -1 && current <= next)        ||  // Descending violation (or equals)
-                    (Math.abs(current - next) > 3))               // Difference too large
-                {
-                    //System.out.println("UNSAFE");
-                    unsafe = 1;
-                    break;
-                }
-            }
-            //if none unsafe checks flagged, it should remain 0
-            if(unsafe == 0) {
-                //System.out.println("SAFE");
+            boolean result = checkLine(report);
+            if(result) {
                 safeCount++;
             }
         }
 
         return safeCount;
+    }
+
+    private boolean checkLine(String line) {
+        //System.out.println(report);
+        //lineCnt++;
+        //if(lineCnt >= 10) break;
+        //break the line into strings for each number
+        String[] values = line.split("\\s+");
+        //set a flag for ascending or descending (-1 decreasing, 1 increasing)
+        int direction;
+        //check first two numbers to check trend
+        if(parseInt(values[0]) < parseInt(values[1])) {
+            //System.out.println("asc");
+            direction = 1;
+        } else {
+            //System.out.println("desc");
+            direction = -1;
+        }
+
+        for(int i = 0; i < values.length - 1; i++) {
+            //convert the number and next one to be compared
+            int current = parseInt(values[i]);
+            int next = parseInt(values[i+1]);
+            //checks for the 4 unsafe scenarios: breaking
+            if ((direction == 1 && current >= next)         ||  // Ascending violation (or equals)
+                    (direction == -1 && current <= next)        ||  // Descending violation (or equals)
+                    (Math.abs(current - next) > 3))               // Difference too large
+            {
+                //System.out.println("UNSAFE");
+                return false;
+            }
+        }
+        //fallback return
+        return true;
     }
 }
