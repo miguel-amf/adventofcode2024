e2d3a8d5798d620ad5b0a7d8535a7dd99c26eae2
Miguel Montagner <dev@miguelmontagner.com>
2025-01-06 17:19:44 +0000

    partial progress on quiz4. added vertical check but missing 1 during test

--- src/main/java/adventofcode/quizcode/Quiz4.java
+++ src/main/java/adventofcode/quizcode/Quiz4.java
@@ -20,10 +20,8 @@ public class Quiz4 implements Quiz{
                 //System.out.println("TESTING I J:" + i + j);
                 if(lines.get(i).charAt(j) == 'X') {
                     //System.out.println("GOT INSIDE IF");
-                    if(checkHorizontal(lines, i, j)) {
-                        System.out.println("FOUND XMAS OR SAMX HORIZONTALLY! i: " + i + " j: " + j);
-                        xmasCount++;
-                    }
+                    xmasCount = xmasCount + checkHorizontal(lines, i, j);
+                    xmasCount = xmasCount + checkVertical(lines, i, j);
                 }
             }
         }
@@ -38,26 +36,68 @@ public class Quiz4 implements Quiz{
         return 0;
     }
 
-    private boolean checkHorizontal(List<String> lines, int i, int j) {
-
+    private int checkHorizontal(List<String> lines, int i, int j) {
+        int count = 0;
         //this could be achieved by going for sub
-        System.out.println("Start testing for: " + lines.get(i).substring(j) +" length: " + lines.get(i).length() + " j value: " + j);
+        //System.out.println("Start testing for: " + lines.get(i).substring(j) +" length: " + lines.get(i).length() + " j value: " + j);
         //first, check if there is enough space for forward xmas, given j position
         if(j<(lines.get(i).length()-4)) {
-            System.out.println("FORWARD: i "+ i + " j " + j + "String to be checked: " + lines.get(i).substring(j,j+4));
+            //System.out.println("FORWARD: i "+ i + " j " + j + "String to be checked: " + lines.get(i).substring(j,j+4));
             if(lines.get(i).substring(j,j+4).equals(("XMAS")))  {
-                return true;
+                count++;
             }
         }
         //check if there is enough space for backwards xmas (samx), given j position
-        System.out.println("BACK TEST:");
+        //System.out.println("BACK TEST:");
         if(j>=4) {
-            System.out.println("BACKWARDS: i "+ i + " j " + j + "String to be checked: " + lines.get(i).substring(j-4,j));
+            //System.out.println("BACKWARDS: i "+ i + " j " + j + "String to be checked: " + lines.get(i).substring(j-4,j));
             if(lines.get(i).substring(j-4,j).equals(("SAMX"))) {
-                return true;
+                count++;
             }
         }
-        return false;
+        if(count!=0) {
+            System.out.println("Returning: " + count);
+        }
+        return count;
+    }
+
+    private int checkVertical(List<String> lines, int i, int j) {
+        int count = 0;
+        String xmasString = "XMAS";
+        //check if there is XMAS upwards
+        //we lock the j (column) and go upwards on the i (line).
+        if(i>=4) {
+            //on the for loops, it will try to falsify the statement of not existing XMAS pattern
+            boolean found = true;
+            // variable l is to keep track of the letter to be checked
+            for(int offset = 0; offset < 4; offset++) {
+                if(lines.get(i-offset).charAt(j) != xmasString.charAt(offset)) {
+                    found = false;
+                    break;
+                }
+            }
+            if(found) {
+                count++;
+            }
+        }
+
+        //check if there is a XMAS downwards
+        if(i<lines.size()-4) {
+            //on the for loops, it will try to falsify the statement of not existing XMAS pattern
+            boolean found = true;
+            // variable l is to keep track of the letter to be checked
+            for(int offset = 0; offset < 4; offset++) {
+                if(lines.get(i+offset).charAt(j) != xmasString.charAt(offset)) {
+                    found = false;
+                    break;
+                }
+            }
+            if(found) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
 
--- src/test/java/adventofcode/quizcode/Quiz4Test.java
+++ src/test/java/adventofcode/quizcode/Quiz4Test.java
@@ -9,14 +9,14 @@ class Quiz4Test {
     @Test
     void solvePart1() {
         Quiz4 quiz = new Quiz4();
-        long result = quiz.solvePart1("input3.txt");
-        assertEquals(5, result);
+        long result = quiz.solvePart1("input4.txt");
+        assertEquals(7, result);
     }
 
     @Test
     void solvePart2() {
         Quiz4 quiz = new Quiz4();
-        long result = quiz.solvePart1("input3.txt");
+        long result = quiz.solvePart2("input4.txt");
         assertEquals(0, result);
     }
 }
